b992a78fa7c2af103ed7850c685a242fdd009d2d
Sonu-7891 <sonuverma789163@gmail.com>
2025-06-23 16:52:42 +0530

    code updated with pdf format for download pdf

--- models/Summary.ts
+++ models/Summary.ts
@@ -6,6 +6,13 @@ const summarySchema = new mongoose.Schema({
   content: { type: String, required: true },
   updatedAt: { type: Date, default: Date.now },
   documentId: { type: String, required: true },
+  metadata: {
+    pageCount: Number,
+    url: String,
+    pdfExpiry: String,
+    duration: Number,
+    name: String,
+  },
 });
 
 export const Summary = mongoose.model("Summary", summarySchema);
