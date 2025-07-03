c973c711b134e2c11a7e7a9ab8dde5ad309413c9
Sonu-7891 <sonuverma789163@gmail.com>
2025-07-03 15:22:57 +0530

    namespace required

--- models/Document.ts
+++ models/Document.ts
@@ -4,7 +4,7 @@ const documentSchema = new mongoose.Schema({
   id: { type: String, required: true, unique: true },
   name: { type: String, required: true },
   uploadedAt: { type: Date, default: Date.now },
-  namespace: { type: String },
+  namespace: { type: String , required: true},
   status: { type: String, default: "completed" },
   microsoftId: { type: String },
   userId: { type: String },
